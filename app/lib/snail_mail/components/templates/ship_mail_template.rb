65a851d20add2adf445b2ebb81a5f8f3470e14b3
S.Navdeep Singh <navdeep13dps@gmail.com>
2025-09-02 19:41:05 +0530

    Updated as Nora said 😭

--- app/lib/snail_mail/components/templates/ship_mail_template.rb
+++ app/lib/snail_mail/components/templates/ship_mail_template.rb
@@ -17,10 +17,10 @@ module SnailMail
           )
 
           render_return_address(5, bounds.top - 5, 190, 90, size: 8, font: "f25")
-          render_destination_address(115, bounds.top - 85, 210, 100, size: 18, valign: :center, align: :left)
-          render_imb(150, 0, 270)
-          render_qr_code(162.5, 25, 45)
-          render_letter_id(225, 25, 8)
+          render_destination_address(85, bounds.top - 75, 260, 130, size: 18, valign: :center, align: :left)
+          render_imb(245, 20, 170)
+          render_qr_code(150, 260, 35)
+          render_letter_id(3, 15, 8, rotate: 90)
           render_postage
         end
       end
